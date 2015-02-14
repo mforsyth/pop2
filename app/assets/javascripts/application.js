c28be5c3a1280c1985caff08d742c4f15948fa9b
Matthew Forsyth <matt@mojotech.com>
2015-02-13 21:16:37 -0500

    Fix menu js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,25 +4,25 @@ var menu_over;
 var submenu_over;
 function mouseOutMenu(menu) { 
 	if (submenu_over != menu) {
-		$('submenu_' + menu).hide();
+		$('#submenu_' + menu).hide();
 	}
 	menu_over = null;
 }
 
 function mouseOverMenu(menu) {
-	$('submenu_' + menu).show();
+	$('#submenu_' + menu).show();
 	menu_over = menu;
 }
 
 function mouseOutSubMenu(menu) { 
 	if (menu_over != menu) {
-		$('submenu_' + menu).hide();
+		$('#submenu_' + menu).hide();
 	}
 	submenu_over = null;	
 }
 
 function mouseOverSubMenu(menu) {
-	$('submenu_' + menu).show();
+	$('#submenu_' + menu).show();
 	submenu_over = menu;	
 }
 
