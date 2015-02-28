20c98fae71a34bd7c87c3391d3ac2b582af1fd70
Matthew Forsyth <matt@mojotech.com>
2015-02-27 21:04:45 -0500

    Add totals to admin.

---
+++ app/admin/totals.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register Setting, as: "Totals" do
+  menu false 
+  actions :show, :edit, :update
+  
+  permit_params :num_orchards_supported
+
+  show title: 'Totals' do
+    attributes_table do
+      row :num_orchards_supported
+      row :orchards_planted
+      row :trees_planted
+      row :berry_bushes_planted, label: 'Shrubs and Vines'
+      row :num_perennials
+    end
+  end
+
+  form do |f|
+    f.inputs('Totals') do
+      f.input :num_orchards_supported
+      f.input :orchards_planted
+      f.input :trees_planted
+      f.input :berry_bushes_planted, label: 'Shrubs and Vines'
+      f.input :num_perennials
+      f.submit(value: 'Update Totals')
+    end
+  end
+end
